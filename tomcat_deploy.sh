e679208b326820399a8acb4d868869e1785e5c31
NaderHammouda <naderhammouda1999@gmail.com>
2022-02-02 23:08:35 +0100

    updates

--- tomcat_deploy.sh
+++ tomcat_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker build -t custom-tomcat .
 
+#Running the dev container on the port 8080
 docker run --name dev-instance -p 8080:8080 -e ENVIRONMENT="dev"  --privileged=true  -dit custom-tomcat
 
+#Running the prod container on the port 8080
 docker run --name prod-instance -p 8080:8080 -e ENVIRONMENT="prod"  --privileged=true  -dit custom-tomcat
